52a9db30f09106df9a2567632c372de39363d321
ReFormationPro <44122582+ReFormationPro@users.noreply.github.com>
2019-07-25 22:19:34 +0300

    Update index.js
    Better functionality

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const CWSC = function(uri, maxAttempts, {onOpen = () => {}, onMessage = () => {}, onClose = () => {}}) {
-    this.maxAttempts = maxAttempts || 1;
+var CWSC = function(uri, maxAttempts = 1, {onOpen = () => {}, onMessage = () => {}, onClose = () => {}} = {}) {
+    this.maxAttempts = maxAttempts;
     this._attempted = 0;
     this._uri = uri;
     
@@ -7,8 +7,35 @@ const CWSC = function(uri, maxAttempts, {onOpen = () => {}, onMessage = () => {}
     this.onUserMessage = onMessage;
     this.onUserClose = onClose;
     
-    this.connect(uri);
+    if (!this.connect(uri)) {
+        console.error("Socket state is CLOSED");
+    }
 }
+
+//Methods
+CWSC.prototype.sendMessage = function(msg) {
+    console.debug("sendMessage", msg);
+    this._ws.send(msg + "\n\x00");
+}
+CWSC.prototype.connect = function(uri) {
+    this._ws = new WebSocket(uri);//"GET"
+    this._attempted++;
+    
+    if (this._ws.readyState != WebSocket.CLOSED) {
+        this._ws.addEventListener("open", this.onOpen.bind(this));
+        this._ws.addEventListener("close", this.onClose.bind(this));
+        this._ws.addEventListener("message", this.onMessage.bind(this));
+        this._ws.addEventListener("error", this.onError.bind(this));
+        return true;
+    } else {
+        return false;
+    }
+}
+CWSC.prototype.disconnect = function() {
+    this._ws.close();
+    //TODO Remove listeners?
+}
+
 //Listeners, don't touch
 CWSC.prototype.onOpen = function(evt) {
     console.debug("onOpen", evt);
@@ -26,27 +53,9 @@ CWSC.prototype.onError = function(evt) {
     console.debug("onError", evt);
     if (this._attempted < this.maxAttempts) {
         console.error("Error: Connection failed. Retrying.");
-        this.attempted++;
         this.connect(this._uri);
     } else {
         console.error("Error: Connection failed. Max connection attempt is reached. Quitting.");
         this.disconnect();
     }
-}
-//Methods
-CWSC.prototype.sendMessage = function(msg) {
-    console.debug("sendMessage", msg);
-    this._ws.send(msg, "\x00");
-}
-CWSC.prototype.connect = function(uri) {
-    this._ws = new WebSocket(uri);
-    
-    this._ws.addEventListener("open", this.onOpen);
-    this._ws.addEventListener("close", this.onClose);
-    this._ws.addEventListener("message", this.onMessage);
-    this._ws.addEventListener("error", this.onError);
-}
-CWSC.prototype.disconnect = function() {
-    this._ws.close();
-    //TODO Remove listeners?
 }
